fc22b9c73587d3e77457c0dc700a29a68c866b52
sidhiek.dms <sidika0363@gmail.com>
2020-12-29 09:21:35 +0700

    get post

---
+++ src/Mahasiswa.js
@@ -0,0 +1,14 @@
+import React, { Component, useState, useEffect } from 'react'
+import {NavigationContainer} from '@react-navigation/native'
+import Routes from './routes'
+
+const Mahasiswa =() =>{
+    
+        return (
+            <NavigationContainer>
+                <Routes></Routes>
+            </NavigationContainer>
+        )
+    }
+export default Mahasiswa
+
